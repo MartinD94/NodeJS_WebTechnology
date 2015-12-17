f27d683a85c738865469998e844ec3e1e627eb23
BuildTools <unconfigured@null.spigotmc.org>
2015-12-17 12:07:40 +0100

    new_function1-v2

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -150,10 +150,10 @@ function deleteUser(event) {
             // Update the table
             populateTable();
 			//Populate Info Box
-    		$('#userInfoName').text();
-	    	$('#userInfoAge').text();
-    		$('#userInfoGender').text();
-    		$('#userInfoLocation').text();
+    		$('#userInfoName').text(" ");
+	    	$('#userInfoAge').text(" ");
+    		$('#userInfoGender').text(" ");
+    		$('#userInfoLocation').text(" ");
 
         });
 
